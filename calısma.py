27134e8ba72ee18f69573eef2789f707a13b0a41
busraugurx <busraugur934@gmail.com>
2021-05-28 00:11:46 +0300

    Initial commit

---
+++ calısma.py
@@ -0,0 +1,8 @@
+print("ARACIN KİLOMETRESİNE GÖRE ÖDEMESİ GEREKEN MİKTAR")
+
+aracın_kilometrede_yaktığı=float(input("ARACINIZ KİLOMETRE BAŞINA NE KADAR YAKIYOR: "))
+aracın_kilometresi=float(input("ARACINIZIN KİLOMETRESİNİ GİRİN: "))
+
+odenecek_miktar= aracın_kilometrede_yaktığı*aracın_kilometresi
+
+print("ÖDENECEK MİKTAR: ",odenecek_miktar)
